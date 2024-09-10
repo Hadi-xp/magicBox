3c95493bcf0928c5ce1936f9098838e89f0b00f0
Hadixp <hadi.b.2002@gmail.com>
2024-09-10 13:15:07 +0430

    added: files

---
+++ main.js
@@ -0,0 +1,64 @@
+const box = document.querySelector(".box");
+const myMusic = document.querySelector(".music audio");
+let currentColor = box.style.background;
+document.addEventListener("keydown",press);
+box.addEventListener("mouseover",hover);
+box.addEventListener("mouseleave",out);
+document.addEventListener("keydown",play);
+document.addEventListener('keydown',move);
+const moveAmount = 100;
+let x = 0;
+let y = 0;
+
+
+function press(event){
+    if(event.keyCode == 49){
+        box.style.background = "red";
+        currentColor = box.style.background;
+    }else if(event.keyCode == 50){
+        box.style.background = "green";
+        currentColor = box.style.background;
+    }else if(event.keyCode == 51){
+        box.style.background = "blue";
+        currentColor = box.style.background;
+    }
+        
+}
+function hover(){
+    box.style.background = "black";
+}
+function out(){
+    box.style.background = currentColor
+}
+
+function play(event){
+    if(event.keyCode == 32){
+        myMusic.play();
+    }
+}
+
+function stop(event){
+    if(event.keyCode == 32){
+        myMusic.pause();
+    }
+}
+function moveRight(event){
+    if(event.keyCode == 39){
+        box.style
+    }
+}
+
+function move(event){
+   if(event.keyCode == 37){
+    x -= moveAmount;
+   }else if (event.keyCode == 39){
+    x += moveAmount;
+   }else if(event.keyCode == 38){
+    y -= moveAmount;
+   }else if (event.keyCode == 40){
+    y += moveAmount
+   }
+
+   box.style.top = `${y}px`;
+   box.style.left = `${x}px`;
+}
